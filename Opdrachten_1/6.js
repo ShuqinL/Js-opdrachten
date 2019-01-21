fa68967b1d66caf8cbc03b05e5c8591d65c4312a
L <reneelsq@hotmail.com>
2019-01-21 12:34:14 +0100

    files added

---
+++ Opdrachten_1/6.js
@@ -0,0 +1,9 @@
+  
+           
+            function myFunction(){
+                var firstNumber=document.getElementById("firstNumber").value;
+                var secondNumber=document.getElementById("secondNumber").value;
+                var myResult=firstNumber%secondNumber;
+          
+                alert(myResult);
+            }
